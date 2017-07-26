3ff663371597401213135e09e1b95ee076dd369d
LosBopfos <christian.lutz.home@gmx.de>
2017-07-26 21:42:41 +0200

    Added turtle.py

---
+++ turtle.py
@@ -0,0 +1,28 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+def square_circ(n):
+	for i in range(n):
+		square()
+		my_turtle.right(11)
+
+def square():
+	for i in range(4):
+		my_turtle.forward(100)
+		my_turtle.right(90)
+
+
+
+def circle():
+    for i in range(500):
+        my_turtle.speed(0)
+        my_turtle.circle(100)
+        my_turtle.left(11)
+        my_turtle.color('red', 'yellow')
+        my_turtle.begin_fill()
+
+    my_turtle.end_fill()
+
+
+square_circ(500)
