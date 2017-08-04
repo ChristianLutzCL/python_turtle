afdd871b55c38bf49b5b3f369c419c6aa81349ff
LosBopfos <christian.lutz.home@gmx.de>
2017-08-04 13:13:09 +0200

    Added color and random_circ()

--- turtle_test.py
+++ turtle_test.py
@@ -1,16 +1,23 @@
 import turtle
+import random
 
 my_turtle = turtle.Turtle()
 
-
 def square_circ(n):
 	for i in range(n):
-		square()
+		my_turtle.color(random.choice(['brown', 'green', 'red', 'orange', 'blue', 'yellow', 'black', 'pink', 'grey', 'silver']))
+		square(100)
 		my_turtle.right(11)
 
-def square():
+def random_circ(n):
+	for i in range(n):
+		my_turtle.color(random.choice(['brown', 'green', 'red', 'orange', 'blue', 'yellow', 'black', 'pink', 'grey', 'silver']))
+		square(random.randrange(10, 200))
+		my_turtle.right(11)
+
+def square(b):
 	for i in range(4):
-		my_turtle.forward(100)
+		my_turtle.forward(b)
 		my_turtle.right(90)
 
 def circle():
@@ -22,7 +29,8 @@ def circle():
         my_turtle.begin_fill()
 
 def turn():
-	left(180)
+	my_turtle.left(180)
+
 
 
 #square_circ(500)
