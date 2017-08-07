7ff147461a2684c2a74fdb6ede7f306f74993b02
LosBopfos <christian.lutz.home@gmx.de>
2017-08-07 20:28:36 +0200

    Update

--- turtle_test.py
+++ turtle_test.py
@@ -8,25 +8,28 @@ def square_circ(n):
 		my_turtle.color(random.choice(['brown', 'green', 'red', 'orange', 'blue', 'yellow', 'black', 'pink', 'grey', 'silver']))
 		square(100)
 		my_turtle.right(11)
+		print(i + 1)
 
 def random_circ(n):
 	for i in range(n):
 		my_turtle.color(random.choice(['brown', 'green', 'red', 'orange', 'blue', 'yellow', 'black', 'pink', 'grey', 'silver']))
 		square(random.randrange(10, 200))
 		my_turtle.right(11)
+		print(i + 1)
 
 def square(b):
 	for i in range(4):
 		my_turtle.forward(b)
 		my_turtle.right(90)
+		print(i + 1)
 
-def circle():
-    for i in range(500):
+def circle(n):
+    for i in range(n):
         my_turtle.speed(0)
         my_turtle.circle(100)
         my_turtle.left(11)
-        my_turtle.color('red', 'yellow')
         my_turtle.begin_fill()
+		print(i + 1)
 
 def turn():
 	my_turtle.left(180)
